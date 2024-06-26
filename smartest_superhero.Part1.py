b00d6e9195e7fcae108585da3335daf63899e1d5
Vadim Nikolaev <vadn@yandex.ru>
2024-06-26 14:49:50 +0300

    Установка модуля requests

---
+++ smartest_superhero.Part1.py
@@ -0,0 +1,10 @@
+#Начало 26.06.2024
+
+#В терминале pip install requests
+import requests
+
+
+def get_the_smartest_superhero() -> str:
+    the_smartest_superhero = ''
+    # ваш код здесь
+    return the_smartest_superhero
